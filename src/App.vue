8b44c893c1bf9bf37b1ed3775bd8cbf6664d3d95
Maxence <levesqu221@campus.unicaen.fr>
2024-01-25 15:15:10 +0100

    feat: chat

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup></script>
 
 <template>
-  
+  <div class="bg-gray-800 text-white h-full">
+    <RouterView />
+  </div>
 
-  <RouterView />
 </template>
 
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,3 +1,21 @@
+<script setup>
+import {ref} from 'vue'
+const messageText = ref('');
+const messageList = ref([]);
+
+const addMessage = ()=>{
+    messageList.value.push(messageText.value);
+    messageText.value="";
+}
+
+</script>
+
 <template>
-    <div class="bg-red-600">Hello Wolrd</div>
+    <div v-for="(message,index) in messageList" class="p-4" :key="index">
+        {{ message }}
+    </div>
+    <div class="flex align-center p-4">
+        <textarea v-model="messageText" name="message" id="message" rows="1" class="text-black rounded-md"></textarea>
+        <button @click="addMessage" class="p-2 bg-blue-600 rounded-md ml-3">Envoyer</button>
+    </div>
 </template>
